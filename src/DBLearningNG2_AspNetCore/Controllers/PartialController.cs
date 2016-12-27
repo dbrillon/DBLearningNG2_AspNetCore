a5288a4faecd083dd3f46f00043dffda100ada7f
David Brillon <david@isaac.ca>
2016-12-27 15:52:17 -0500

    Modification following the pluralsight course

---
+++ src/DBLearningNG2_AspNetCore/Controllers/PartialController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace DBLearningNG2_AspNetCore.Controllers
+{
+  public class PartialController : Controller
+  {
+    public IActionResult Message() => PartialView();
+
+    public IActionResult Numbers() => PartialView();
+  }
+}
--- src/DBLearningNG2_AspNetCore/Startup.cs
+++ src/DBLearningNG2_AspNetCore/Startup.cs
@@ -1,34 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Glimpse;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
-using Microsoft.AspNet.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
 
 namespace DBLearningNG2_AspNetCore
 {
-    public class Startup
+  public class Startup
+  {
+    // Entry point for the application.
+    public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+
+        public Startup(IHostingEnvironment env)
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
+      // Set up configuration sources.
+      var builder = new ConfigurationBuilder()
+          .AddJsonFile("appsettings.json")
+          .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app)
-        {
-            app.UseIISPlatformHandler();
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
-        }
-
-        // Entry point for the application.
-        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+      builder.AddEnvironmentVariables();
+      Configuration = builder.Build();
     }
+
+    public IConfigurationRoot Configuration { get; set; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddGlimpse();
+      services.AddMvc();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app,
+                          IHostingEnvironment env,
+                          ILoggerFactory loggerFactory)
+    {
+      loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+      loggerFactory.AddDebug();
+
+      if (env.IsDevelopment())
+      {
+        app.UseBrowserLink();
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        app.UseExceptionHandler("/Home/Error");
+      }
+
+      app.UseIISPlatformHandler(options =>
+                                options.AuthenticationDescriptions.Clear());
+      app.UseStaticFiles();
+      app.UseGlimpse();
+
+      app.UseMvc(routes =>
+      {
+        routes.MapRoute("default",
+                        "{controller=Home}/{action=Index}/{id?}");
+        routes.MapRoute("spa-fallback",
+                        "{*anything}",
+                        new { controller = "Home", action = "Index" });
+        routes.MapWebApiRoute("defaultApi",
+                              "api/{controller}/{id?}");
+      });
+    }
+  }
 }
